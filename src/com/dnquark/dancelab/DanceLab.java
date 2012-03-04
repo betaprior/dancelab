012d11661dcc5d43f7c730c83bcd0ed524167058
Leo Alekseyev <leo.alekseyev+begemot@gmail.com>
2012-03-04 13:46:09 -0500

    Data logging in a HandlerThread; file listing in the UI

--- src/com/dnquark/dancelab/DanceLab.java
+++ src/com/dnquark/dancelab/DanceLab.java
@@ -6,7 +6,9 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import android.app.Activity;
 import android.content.Context;
@@ -16,9 +18,13 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.view.View;
 //import android.widget.EditText;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.util.Log;
@@ -27,10 +33,12 @@ import android.util.Log;
 public class DanceLab extends Activity {
 
 	private static final String TAG = "DanceLab";  
+	private static boolean DEBUG_FILENAME = true;
 
     private boolean appendTimestamps = false;
 	private TextView statusText, fnameText;
 	private CheckBox checkBoxTS;
+	private FileManager fileManager;
 	private DataLogger logger;
 	
     /** Called when the activity is first created. */
@@ -42,7 +50,9 @@ public class DanceLab extends Activity {
         statusText = (TextView) findViewById(R.id.recordingStatus1);
         fnameText = (TextView) findViewById(R.id.textViewFname1);
         checkBoxTS = (CheckBox) findViewById(R.id.checkBoxTimestamps1);
-        logger = new DataLogger();
+        fileManager = new FileManager();
+	logger = new DataLogger();
+	updateFileList();
         
         Thread.setDefaultUncaughtExceptionHandler(new CustomExceptionHandler(
                 "/sdcard/dancelab/", null));
@@ -68,6 +78,7 @@ public class DanceLab extends Activity {
 		case R.id.stopButton1:
 		    logger.stopLogging();
 			statusText.setText("stopped");
+			updateFileList();
 			break;
 		case R.id.checkBoxTimestamps1:
 			if (((CheckBox)view).isChecked()) 
@@ -77,6 +88,17 @@ public class DanceLab extends Activity {
 		}
 	}
 	
+	public void updateFileList() {
+		ListView listView = (ListView) findViewById(R.id.filelist);
+		List<String> filenames = fileManager.getFileListing();
+		if (filenames == null) return;
+    ArrayAdapter<String> fileList =
+  	      new ArrayAdapter<String>(this, R.layout.row, filenames);
+
+  	     listView.setAdapter(fileList);
+
+  	    }
+
 	
 	private void displayToast(String msg) {
 	         Toast.makeText(getBaseContext(), msg, 
@@ -86,49 +108,78 @@ public class DanceLab extends Activity {
 	 @Override
 	 protected void onStop() {
 	     super.onStop();
-	     try {
-		 logger.stopLogging();
-	     } catch (NullPointerException e) {
-		 Log.e(TAG," caught NPE");
-	     }
+	     logger.stopLogging();
 	 }	
 	    
-public class DataLogger implements SensorEventListener {
+	 
+	protected class FileManager {
+	    public static final String SAVE_FILEDIR = "dancelab";
+	    public static final String SAVE_FILENAME_BASE = "dancelab";
+	    private static final String SAVE_FILENAME_BASE_TS = "dl";
+	    private File root, storeDir;
+	    private File[] files;
+	    private List<String> filenames;
+	    
+	    public FileManager() {
+	    root = Environment.getExternalStorageDirectory();
+	    storeDir = new File(root, FileManager.SAVE_FILEDIR);
+	    if (! storeDir.isDirectory()) 
+		storeDir.mkdir();
+	    	filenames = new ArrayList<String>();
+	    }
+	    public File getDataDir() { return storeDir; } 
+	    public List<String> getFileListing() {
+	    	files = storeDir.listFiles();
+	    	for (int i=0; i < files.length; i++) 
+	    		filenames.add(files[i].getName());
+	    	return filenames;
+	    }
+	    	
+	   
+	}
+
+	 
+    protected class DataLogger implements SensorEventListener {
     private static final String TAG = "DanceLabLogger";
-    public static final String SAVE_FILEDIR = "dancelab";
-    public static final String SAVE_FILENAME_BASE = "dancelab";
-    private static final String SAVE_FILENAME_BASE_TS = "dl";
-    private String metaFilename, saveFilename = SAVE_FILENAME_BASE;
+    private String metaFilename, saveFilename = FileManager.SAVE_FILENAME_BASE;
     private static final int NDIMS = 3;
     private float[] accelVals, gyroVals;
-    
-    private SensorManager mSensorManager;
-    
-    private int idx = 0;
-    private File outfile, outfileMeta;
-    private FileWriter outfileWriter, outfileWriterMeta;
-    private BufferedWriter outfileBWriter, outfileBWriterMeta;
-    private Long tStart, tStop;
     private boolean loggingIsOn = false;
     
-    public DataLogger() {
+    private SensorManager mSensorManager;
+	private HandlerThread mHandlerThread;
+	private Handler handler;
+
+    private int idx = 0;
+    private File outfile, outfileMeta;
+    private BufferedWriter outfileBWriter, outfileBWriterMeta;
+    private Long tStart, tStop;
+    
+	public DataLogger() { 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 	accelVals = new float[NDIMS];
 	gyroVals = new float[NDIMS];
 	for (int i = 0; i < NDIMS; i++)
 	    accelVals[i] = gyroVals[i] = 0;
+
+
     }
 
-    public void startLogging() {
+	protected void startLogging() {
+		mHandlerThread = new HandlerThread("sensorThread");
+	    mHandlerThread.start();
+	    handler = new Handler(mHandlerThread.getLooper());
 	registerListeners();
 	prepFileIO();
 	tStart = System.currentTimeMillis();
 	writeMetadataStart();
 	loggingIsOn = true;
-    }
+	}
     
-    public void stopLogging() {
+    protected void stopLogging() {
+    	if (!loggingIsOn) return;
 	loggingIsOn = false;
+	mHandlerThread.quit();
 	unregisterListeners();
 	tStop = System.currentTimeMillis();
 	writeMetadataEnd();
@@ -138,13 +189,13 @@ public class DataLogger implements SensorEventListener {
     public void registerListeners() {
     	mSensorManager.registerListener(this,
     			mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-    			SensorManager.SENSOR_DELAY_FASTEST);
+    			SensorManager.SENSOR_DELAY_FASTEST, handler);
     	//		    mSensorManager.registerListener(this,
     	//	                mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR),
     	//	                SensorManager.SENSOR_DELAY_FASTEST);
-    	mSensorManager.registerListener(this,	
-    			mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE),
-    			SensorManager.SENSOR_DELAY_FASTEST);
+//    	mSensorManager.registerListener(this,	
+//    			mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE),
+//					SensorManager.SENSOR_DELAY_FASTEST, handler);
     }
     
     public void unregisterListeners() {
@@ -159,7 +210,7 @@ public class DataLogger implements SensorEventListener {
     }
 
     public void writeMetadataEnd() {
-	long diffStartStop = tStart - tStop;
+	long diffStartStop = tStop - tStart;
 	float msPerSample = diffStartStop/(idx+1);
 	try {
 	    outfileBWriterMeta.write("stopped @" + Long.toString(tStop));
@@ -180,18 +231,15 @@ public class DataLogger implements SensorEventListener {
 
     private void prepFileIO() {
 	try {
-	    File root = Environment.getExternalStorageDirectory();
-	    File storeDir = new File(root, SAVE_FILEDIR);
-	    if (! storeDir.isDirectory()) 
-		storeDir.mkdir();
+		File storeDir = fileManager.getDataDir();
 	    if (storeDir.canWrite()) {
-		saveFilename = SAVE_FILENAME_BASE_TS + "-" + getTimestamp();
+		saveFilename = FileManager.SAVE_FILENAME_BASE_TS + "-" + getTimestamp();
+		if (DanceLab.DEBUG_FILENAME) saveFilename = "test";
 		metaFilename = saveFilename + ".nfo";
 		outfile = new File(storeDir, saveFilename);
 		outfileMeta = new File(storeDir, metaFilename);
 		fnameText.setText(outfile.getPath());
-		outfileWriter = new FileWriter(outfile);
-		outfileBWriter = new BufferedWriter(outfileWriter);
+		outfileBWriter = new BufferedWriter(new FileWriter(outfile));
 		outfileBWriterMeta = new BufferedWriter(new FileWriter(outfileMeta));
 		fnameText.setText(" dir: " + storeDir.getPath());
 	    } else {
@@ -203,7 +251,7 @@ public class DataLogger implements SensorEventListener {
     }
 
     public void onSensorChanged(SensorEvent event) {
-	Log.d(TAG, "sensor: " + event.sensor + ", x: " + event.values[0] + ", y: " + event.values[1] + ", z: " + event.values[2]);
+	Log.d(TAG, "idx = " + idx + "sensor: " + event.sensor + ", x: " + event.values[0] + ", y: " + event.values[1] + ", z: " + event.values[2]);
 	synchronized (this) {			  
 	    switch (event.sensor.getType()) {
 	    case Sensor.TYPE_ACCELEROMETER:
@@ -235,7 +283,7 @@ public class DataLogger implements SensorEventListener {
 	    accelMagnitude = 0;
 	    for (i = 0; i < NDIMS; i++) { 
 		outfileBWriter.write(Float.toString(accelVals[i]) + ",");
-		accelMagnitude += accelVals[i];
+		accelMagnitude += accelVals[i] * accelVals[i];
 	    }
 	    accelMagnitude = (float) Math.sqrt(accelMagnitude);
 	    outfileBWriter.write(Float.toString(accelMagnitude) + ",");
